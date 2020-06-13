84ad4cbb01bad1a55ff24b11d66d9b8ccd689dc5
Luis Padron <heyluispadron@gmail.com>
2020-06-13 19:49:58 -0400

    add Player animation

--- Sources/Player.gd
+++ Sources/Player.gd
@@ -6,18 +6,19 @@ const MAX_SPEED = 90
 
 # -- The current velocity vector for the 'Player'.
 var velocity = Vector2.ZERO
-# -- The 'AnimationPlayer' responsible for animating the 'Player'.
+# -- The 'AnimationPlayer' responsible for this 'Player'.
 onready var animation_player = $AnimationPlayer
-# -- The 'AnimationTree' responsible for storage of the animations for Player'.
-onready var animation_tree = $AnimationTree
 
-func _physics_process(delta):
+func _physics_process(delta: float):
 	var input_v = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	).normalized()
-	
-	animate(input_v)
+
+	if input_v.x > 0:
+		animation_player
+	else:
+
 	
 	if input_v == Vector2.ZERO:
 		# Slow down player
@@ -28,15 +29,6 @@ func _physics_process(delta):
 
 	velocity = move_and_slide(velocity)
 
-# -- Animates the 'Player' sprite depending on current direction.
-func animate(input_v: Vector2):
-	var animation_state = animation_tree.get("parameters/playback")
-	
-	if input_v == Vector2.ZERO:
-		animation_state.travel("Idle")
-	else:
-		animation_state.travel("Run")	
-		animation_tree.set("parameters/Idle/blend_position", input_v)
-		animation_tree.set("parameters/Run/blend_position", input_v)
-	
-	
+func animate(input_v: Vector2D):
+	if input_v.x > 0:
+		animation_player.play("")
